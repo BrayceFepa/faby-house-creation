8dc84f5db8e9d7d73fcb4b5747db54d1fa43d185
brayce <braycefepa@gmail.com>
2023-07-02 03:29:49 +0100

    designing and api integrration

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ origin: true }));
 
 app.use(express.json({ limit: "4mb" }));
 
