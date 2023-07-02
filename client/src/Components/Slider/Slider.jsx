c1cca8381266494a03a6cf318091e0cd054f73b1
brayce <braycefepa@gmail.com>
2023-07-02 03:43:36 +0100

    updating

--- client/src/Components/Slider/Slider.jsx
+++ client/src/Components/Slider/Slider.jsx
@@ -43,8 +43,8 @@ const Slider = () => {
           <div className="slide">
             <div className="image">
               <picture>
-  <source srcset={images.mobileSlide1} media="(max-width: 768px)"/>
-  <img src={images.slide1} alt="Default Image"/>
+  <source srcset={"https://faby-house-creation-16dl.vercel.app/static/media/mobile-banner.831b8e56756bec4a43b6.jpg"} media="(max-width: 768px)"/>
+  <img src={"https://faby-house-creation-16dl.vercel.app/static/media/banner-v1.cb9ba1b36c1516e16061.jpg"} alt="Default Image"/>
 </picture>
             {/* <img src= alt="" /> */}
           </div>
@@ -55,8 +55,8 @@ const Slider = () => {
           <div className="slide">
             <div className="image">
               <picture>
-  <source srcset={images.mobileSlide2} media="(max-width: 768px)"/>
-  <img src={images.slide2} alt="Default Image"/>
+  <source srcset={"https://faby-house-creation-16dl.vercel.app/static/media/mobile-banner-2.c8419f1da52cde3f382f.jpg"} media="(max-width: 768px)"/>
+  <img src={"https://faby-house-creation-16dl.vercel.app/static/media/banner-v2.dd1e45bb15459495aa32.jpg"} alt="Default Image"/>
 </picture>
             {/* <img src={images.slide2} alt="" /> */}
           </div>
@@ -67,8 +67,8 @@ const Slider = () => {
           <div className="slide">
             <div className="image">
               <picture>
-  <source srcset={images.mobileSlide3} media="(max-width: 768px)"/>
-  <img src={images.slide3} alt="Default Image"/>
+  <source srcset={"https://faby-house-creation-16dl.vercel.app/static/media/mobile-banner-3.0111d15ed852ee18ad5d.jpg"} media="(max-width: 768px)"/>
+  <img src={"https://faby-house-creation-16dl.vercel.app/static/media/banner-v3.6e39d01bcd0b4f8720d1.jpg"} alt="Default Image"/>
 </picture>
             {/* <img src={images.slide3} alt="" /> */}
           </div>
